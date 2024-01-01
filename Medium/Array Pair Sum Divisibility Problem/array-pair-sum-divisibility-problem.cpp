2351e2fc1fc53115da91d27958617623683f489e
anuja2004 <126876878+anuja2004@users.noreply.github.com>
2024-01-01 22:15:09 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Array Pair Sum Divisibility Problem/array-pair-sum-divisibility-problem.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    bool canPair(vector<int> nums, int k) {
+        // Code here.
+         if (nums.size() % 2 != 0) {
+        return false;
+    }
+
+    unordered_map<int, int> remainderFreq;
+
+    for (int num : nums) {
+        int remainder = (num % k + k) % k; // Ensure positive remainder
+        remainderFreq[remainder]++;
+    }
+
+    if (remainderFreq[0] % 2 != 0) {
+        return false;
+    }
+
+    for (int i = 1; i <= k / 2; ++i) {
+        if (remainderFreq[i] != remainderFreq[k - i]) {
+            return false;
+        }
+    }
+
+    return true;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n, k;
+        cin >> n >> k;
+        vector<int> nums(n);
+        for (int i = 0; i < nums.size(); i++) cin >> nums[i];
+        Solution ob;
+        bool ans = ob.canPair(nums, k);
+        if (ans)
+            cout << "True\n";
+        else
+            cout << "False\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
